254927091337eb1a21e4a4aa06d122f5340f1ac7
Morteza Nokhodian <nokhodian@gmail.com>
2026-03-25 17:44:22 +0000

    Update monotask CLI to v0.3.8

--- Formula/monotask.rb
+++ Formula/monotask.rb
@@ -1,23 +1,23 @@
 class Monotask < Formula
   desc "P2P Kanban CLI – local-first task management with collaboration"
   homepage "https://github.com/nokhodian/monotask"
-  version "0.3.6"
+  version "0.3.8"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/nokhodian/monotask/releases/download/v0.3.6/monotask-v0.3.6-aarch64-apple-darwin.tar.gz"
-      sha256 "dca84554a4457caa1f3bb3398d76568980045765c8ce745b101cf5ed3fc42a1d"
+      url "https://github.com/nokhodian/monotask/releases/download/v0.3.8/monotask-v0.3.8-aarch64-apple-darwin.tar.gz"
+      sha256 "0f815502221b7abbce3bc6879e8b93b95e11426fa4d8effe002e7279bc1e17c1"
     end
     on_intel do
-      url "https://github.com/nokhodian/monotask/releases/download/v0.3.6/monotask-v0.3.6-x86_64-apple-darwin.tar.gz"
-      sha256 "f28a64e97e401b024f59fd2bbd72919eb805f39a9a8da683bd851cec38a395b9"
+      url "https://github.com/nokhodian/monotask/releases/download/v0.3.8/monotask-v0.3.8-x86_64-apple-darwin.tar.gz"
+      sha256 "177fe34ad026e8d2d4e4b7a5fe68bea982fd723fcdebf07ddf7efa54a8ddd778"
     end
   end
 
   on_linux do
-    url "https://github.com/nokhodian/monotask/releases/download/v0.3.6/monotask-v0.3.6-x86_64-linux.tar.gz"
-    sha256 "1c23c4533f7c278835110f8d7109ca8615082cad3d68055fb62e642833038a2a"
+    url "https://github.com/nokhodian/monotask/releases/download/v0.3.8/monotask-v0.3.8-x86_64-linux.tar.gz"
+    sha256 "0e53bbbe4d89c6f38d32801ee05a055ad0e68c1c34e57ea48ef1c2fe345a766f"
   end
 
   def install
