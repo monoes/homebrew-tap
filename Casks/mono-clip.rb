70e9a508e2d468492b824817449941489e5fc032
Morteza Nokhodian <nokhodian@gmail.com>
2026-03-25 09:19:31 +0100

    fix: correct Ruby interpolation in cask URL

--- Casks/mono-clip.rb
+++ Casks/mono-clip.rb
@@ -2,7 +2,7 @@ cask "mono-clip" do
   version "0.2.2"
   sha256 "d560262a45c1f4f9cd97d1329b71537281cc2af6b31fa069072973557391df3b"
 
-  url "https://github.com/nokhodian/mono-clip/releases/download/v\#{version}/MonoClip_\#{version}_aarch64.dmg"
+  url "https://github.com/nokhodian/mono-clip/releases/download/v#{version}/MonoClip_#{version}_aarch64.dmg"
   name "MonoClip"
   desc "Blazing-fast macOS clipboard manager with folder organization, CLI, and AI integration"
   homepage "https://github.com/nokhodian/mono-clip"
