d682420f47267ad91a02fd9a636dd094b5f9be03
Morteza Nokhodian <nokhodian@gmail.com>
2026-04-01 14:11:24 +0000

    Update Monotask desktop app to v0.3.19

--- Casks/monotask.rb
+++ Casks/monotask.rb
@@ -1,8 +1,8 @@
 cask "monotask" do
-  version "0.3.18"
-  sha256 "5beab129359d1e7fc265de7e0941d45528f6d5cad0dac9e8f916ff32a70989aa"
+  version "0.3.19"
+  sha256 "f1058278418aecf866998c3814170e71b1ee6510b27eafbfc9332e01995bc223"
 
-  url "https://github.com/nokhodian/monotask/releases/download/v0.3.18/Monotask-v0.3.18-aarch64.dmg"
+  url "https://github.com/nokhodian/monotask/releases/download/v0.3.19/Monotask-v0.3.19-aarch64.dmg"
   name "Monotask"
   desc "Local-first peer-to-peer kanban board with cryptographic identity"
   homepage "https://github.com/nokhodian/monotask"
