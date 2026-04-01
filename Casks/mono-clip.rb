e6111eacf075434622f3a2f5ad0a6af71f9c7f56
nokhodian <nokhodian@gmail.com>
2026-04-01 22:15:41 +0200

    bump mono-clip to 0.2.5

--- Casks/mono-clip.rb
+++ Casks/mono-clip.rb
@@ -1,6 +1,6 @@
 cask "mono-clip" do
-  version "0.2.4"
-  sha256 "ffe64e40c73cac5a8839555e24873c73c0f1bd1e24ff349b425cea950c824b4a"
+  version "0.2.5"
+  sha256 "94150d1ea2153938cc0228b11299c837e916aa9efd21c3a03569d7a5dc93f461"
 
   url "https://github.com/nokhodian/mono-clip/releases/download/v#{version}/MonoClip_#{version}_aarch64.dmg"
   name "MonoClip"
