0d5620bf784ac1c5a2a2b4e890c08880a96821e5
Morteza Nokhodian <nokhodian@gmail.com>
2026-04-16 23:52:54 +0200

    chore: update org nokhodian → monoes

--- Casks/monotask.rb
+++ Casks/monotask.rb
@@ -2,10 +2,10 @@ cask "monotask" do
   version "0.4.4"
   sha256 "a92d923226f0cb0422f3b6e0bef861487e66dd63eb0b7c8006604f224fe3cb81"
 
-  url "https://github.com/nokhodian/monotask/releases/download/v0.4.4/Monotask-v0.4.4-aarch64.dmg"
+  url "https://github.com/monoes/monotask/releases/download/v0.4.4/Monotask-v0.4.4-aarch64.dmg"
   name "Monotask"
   desc "Local-first peer-to-peer kanban board with cryptographic identity"
-  homepage "https://github.com/nokhodian/monotask"
+  homepage "https://github.com/monoes/monotask"
 
   depends_on macos: ">= :ventura"
 
