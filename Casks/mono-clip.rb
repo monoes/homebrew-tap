c6f5dce7c6ee5b3d3cce4a7d4c89782d6833cfd8
nokhodian <nokhodian@gmail.com>
2026-09-13 11:37:57 +0200

    chore: bump mono-clip to 0.2.19

--- Casks/mono-clip.rb
+++ Casks/mono-clip.rb
@@ -1,6 +1,6 @@
 cask "mono-clip" do
-  version "0.2.18"
-  sha256 "a737348da1a31f411a5b6b589cf2b36fb0bbe3cfa6b412bccaf8315b7776f01a"
+  version "0.2.19"
+  sha256 "58c30d6c7b8dd9c18d2296942d7315aba4982d561a0aae1bc9b9d46ea57112e0"
 
   url "https://github.com/monoes/mono-clip/releases/download/v#{version}/MonoClip_#{version}_aarch64.dmg"
   name "MonoClip"
